3046e182119094125ef5c42ebd25b1cdba429c7c
samsunghappytree123 <samsunghappytree123@naver.com>
2023-02-05 20:45:08 +0900

    feat: TossFace 사용, 유저 프로필 일부 추가

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,8 +3,20 @@ import React from 'react';
 const Index = () => {
     return (
         <>
-            <div className='userhub'>
-                <p>Hello, World!</p>
+            <div className='user_profile'>
+                <div className='user_profile_image'>
+                    <img src='/favicon-192x192.png' alt='logo' width='296px' height='296px' style={{borderRadius: '50%'}}/>
+                    <div>
+                        <h2>Yunseo Jeong</h2>
+                        <p style={{color: '#B1B5B0', marginTop: '-15px'}}>samsunghappytree123</p>
+                        <p>👋 Hello, World!</p>
+                    </div>
+                </div>
+                <div className='user_profile_overview'>
+                    <h1>Yunseo Jeong</h1>
+                    <h2>😜 안녕하세요, 삼해트입니다.</h2>
+                    <p>🚧 현재 이 페이지는 준비중입니다 :)</p>
+                </div>
             </div>
         </>
     )
