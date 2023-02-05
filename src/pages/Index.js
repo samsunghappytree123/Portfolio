8a2d4023b3dc92699bb96a2269146e9677fb334a
samsunghappytree123 <samsunghappytree123@naver.com>
2023-02-05 22:31:40 +0900

    feat: 간단 소개말 추가

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,10 +12,26 @@ const Index = () => {
                         <p>👋 Hello, World!</p>
                     </div>
                 </div>
-                <div className='user_profile_overview'>
-                    <h1>Yunseo Jeong</h1>
-                    <h2>😜 안녕하세요, 삼해트입니다.</h2>
-                    <p>🚧 현재 이 페이지는 준비중입니다 :)</p>
+                <div className='user_profile_overview' id="overview">
+                    <p style={{textAlign: 'left', marginLeft: '20px'}}><code>samsunghappytree123/README.md</code></p>
+                    <hr />
+                    <div className='text'>
+                        <h1># Yunseo Jeong</h1>
+                        <hr style={{marginTop: '-10px', marginBottom: '-10px'}} />
+                        <h2>😜 안녕하세요, 삼해트입니다.</h2>
+                        <p>
+                            저는 이것저것 개발하면서 즐기고 있는 학생 개발자입니다.<br />
+                        </p>
+
+                        <h2>## 나만의 깃허브 잔디밭</h2>
+                        <hr style={{marginTop: '-10px'}} />
+                        <img src="https://ghchart.rshah.org/samsunghappytree123" alt='github_commit_graph' width="100%" height="auto" /><br />
+                        <img src='https://github-readme-stats.vercel.app/api?username=samsunghappytree123&show_icons=true&theme=transparent' alt='github_stats' width="50%" height="auto" />
+                        {/* <img style={{float: 'left'}} src='https://github-readme-stats.vercel.app/api?username=samsunghappytree123&show_icons=true&theme=transparent' alt='github_stats' width="50%" height="auto" /> */}
+                        {/* <img style={{float: 'right'}} src='https://github-readme-stats.vercel.app/api/top-langs/?username=samsunghappytree123&show_icons=true&theme=transparent&langs_count=4' alt='github_stats' width="50%" height="auto" /> */}
+
+                        <p>➡ 저에 대한 더 자세한 정보는 아래의 레포지토리 목록을 확인해주세요!</p>
+                    </div>
                 </div>
             </div>
         </>
