b888decc5beb305c3592ab98c3201a0ad59133ce
Yunseo Jeong <samsunghappytree123@naver.com>
2023-02-07 18:17:15 +0900

    feat: 404 임시 새단장

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 const Index = () => {
-    return (
-        <header className='App-header'>
-            <h1>페이지가 존재하지 않아요.</h1>
-            <Link to='/'>홈으로</Link>
-        </header>
-    )
+	const history = useNavigate();
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				minHeight: '90vh'
+			}}
+		>
+			<div>
+				<h1>🚧 404 🚧</h1>
+				<h2>요청하신 페이지가 존재하지 않습니다.</h2>
+				<p>이 페이지가 이동되었거나, 존재하지 않는 것 같아요.</p>
+				<button as={Link} onClick={()=>{history(-1)}} variant="info"><FontAwesomeIcon icon={faArrowLeft} /> 이전 페이지로 이동하기</button> <Link to="/"><button as={Link} variant="primary"><FontAwesomeIcon icon={faHouse} /> 메인 페이지로 이동하기</button></Link>
+			</div>
+		</div>
+	);
 };
 
 export default Index;
