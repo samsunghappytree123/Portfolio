c6507165784b0da34796642a217801989a40fef1
samsunghappytree123 <samsunghappytree123@naver.com>
2023-02-17 13:00:07 +0900

    feat: divide repo content

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,11 +9,12 @@ const About = () => {
         <>
             <Helmet>
 				<title>About - Yunseo Jeong</title>
-				<meta property="og:title" content="About (브랜치 : main) - Yunseo Jeong" />
+				<meta property="og:title" content="About - Yunseo Jeong" />
 			</Helmet>
 
             <div className='repo_header'>
                 <h2><FontAwesomeIcon icon={faBook} /> <Link to="/">YSJ</Link> / <Link to="/About">About</Link> <span className='badge'>Public</span></h2>
+                <div className='repo_sidebar_m'>여기는 설명이 올 예정입니다. (모바일 한정)</div>
             </div>
 
             <div className='repo_tab'>
@@ -22,6 +23,10 @@ const About = () => {
             <div className='repo_tab_hr'>
                 <div className='repo_tab_hr1'><hr /></div>
                 <div className='repo_tab_now'></div>
+            </div>
+            <div className='repo_page'>
+                <div className='repo_content'>test</div>
+                <div className='repo_sidebar'>여기는 설명이 올 예정입니다. (PC 한정)</div>
             </div>
         </>
     )
