961fb47c131fe59ba0d1042f994978ab65a5fce9
samsunghappytree123 <samsunghappytree123@naver.com>
2023-06-17 22:05:00 +0900

    feat: use function in Date

--- src/pages/[root]/index.js
+++ src/pages/[root]/index.js
@@ -9,58 +9,68 @@ import rehypeRaw from "rehype-raw";
 import remarkGfm from 'remark-gfm';
 export const runtime = 'experimental-edge';
 
+function dayCheck(wantDate) {
+  var now = new Date();
+  var writeDay = new Date(wantDate);
+
+  let ResultTime = "";
+
+  var difference = now.getTime() - writeDay.getTime();
+  difference = Math.trunc(difference / 1000);
+
+  const seconds = 1;
+  const minute = seconds * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const mon = day * 30;
+  const year = mon * 12;
+
+  let resultYear, resultMon, resultDay, resultHour, resultMin, resultSec;
+
+  resultYear = Math.trunc(difference / year);
+  resultMon = Math.trunc(difference / mon);
+  resultDay = Math.trunc(difference / day);
+  resultHour = Math.trunc(difference / hour);
+  resultMin = Math.trunc(difference / minute);
+  resultSec = Math.trunc(difference / seconds);
+
+  if(resultSec > 0){
+      ResultTime = resultSec+'초';
+      difference = difference - (resultSec * seconds );
+  }
+
+  if(resultMin > 0){
+      ResultTime = resultMin+'분';
+      difference = difference - (resultMin * minute);
+  }
+
+  if(resultHour > 0){
+      ResultTime = resultHour+'시간';
+      difference = difference - (resultHour * hour);
+  }
+
+  if(resultDay > 0){
+      ResultTime = resultDay+'일';
+      difference = difference - (resultDay * day);
+  }
+
+  if(resultMon > 0){
+      ResultTime = resultMon+'개월';
+      difference=  difference - (resultMon * mon);
+  }
+
+  if(resultYear > 0){
+    ResultTime = resultYear+'년';
+    difference= difference - (resultYear * year);
+  }
+
+  return ResultTime;
+}
+
 export default function ResolveRoute({data, url, title}) {
   if (data === undefined) {
     return <Error statusCode={404} />
   } else {
-    var now = new Date(); 
-    var writeDay = new Date(data.writeDay);
-    
-    let ResultTime = "";
-    
-    var difference = now.getTime() - writeDay.getTime();
-    difference = Math.trunc(difference / 1000);
-    
-    const seconds = 1;
-    const minute = seconds * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const mon = day * 30;
-    const year = mon * 12;
-    
-    let resultYear, resultMon, resultDay, resultHour, resultMin, resultSec;
-
-    resultYear = Math.trunc(difference / year);
-    resultMon = Math.trunc(difference / mon);
-    resultDay = Math.trunc(difference / day);
-    resultHour = Math.trunc(difference / hour);
-    resultMin = Math.trunc(difference / minute);
-    resultSec = Math.trunc(difference / seconds);
-    if(resultSec > 0){
-        ResultTime = resultSec+'초';
-        difference = difference - (resultSec * seconds );
-    }
-    if(resultMin > 0){
-        ResultTime = resultMin+'분';
-        difference = difference - (resultMin * minute);
-    }
-    if(resultHour > 0){
-        ResultTime = resultHour+'시간';
-        difference = difference - (resultHour * hour);
-    }
-    if(resultDay > 0){
-        ResultTime = resultDay+'일';
-        difference = difference - (resultDay * day);
-    }
-    if(resultMon > 0){
-        ResultTime = resultMon+'개월';
-        difference=  difference - (resultMon * mon);
-    }
-    if(resultYear > 0){
-      ResultTime = resultYear+'년';
-      difference= difference - (resultYear * year);
-    }
-
     const star = 0;
     return (
       <>
@@ -101,7 +111,7 @@ export default function ResolveRoute({data, url, title}) {
                       <div className='table_text'>
                           <img src='/favicon-192x192.png' alt='logo' width='32px' height='32px' style={{borderRadius: '50%'}}/> <Link href="/">YSJ</Link> <p style={{marginLeft: '10px'}}>{data.commitMessage}</p>
                       </div>
-                      <div style={{textAlign: 'right', marginTop: '15px', marginLeft: '8px', marginRight: '8px'}}>{ResultTime} 전</div>
+                      <div style={{textAlign: 'right', marginTop: '15px', marginLeft: '8px', marginRight: '8px'}}>{dayCheck(data.writeDay)} 전</div>
                     </th>
                   </tr>
                 </thead>
@@ -119,7 +129,7 @@ export default function ResolveRoute({data, url, title}) {
                         </td>
                         <td style={{minWidth: '85px', width: '20%', color: '#8B949E', borderLeft: 'none', marginTop: '8px', marginBottom: '8px'}}>
                           <div style={{textAlign: 'right', marginLeft: '8px', marginRight: '8px'}}>
-                              {ResultTime} 전
+                            {dayCheck(file.writeDay)} 전
                           </div>
                         </td>
                       </tr>
@@ -193,7 +203,7 @@ export async function getServerSideProps(context) {
     return {props: {}}
   } else {
     return {
-      props: {data: data[context.params.root], url: context.params.root, title: `${data[context.params.root]['name']} - Yunseo Jeong`}
+      props: {data: data[context.params.root], url: context.params.root, title: `${data[context.params.root]['name']} - Yunseo Jeong`},
     };
   }
 }
