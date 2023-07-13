31ecca3aa42a6a85f0dab29181a62b83d5c7188a
samsunghappytree123 <samsunghappytree123@naver.com>
2023-07-13 20:57:44 +0900

    feat: use dayCheck lib

--- src/pages/[root]/[file].js
+++ src/pages/[root]/[file].js
@@ -7,64 +7,8 @@ import { faBook, faCode, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from "rehype-raw";
 import remarkGfm from 'remark-gfm';
+import * as checkScript from '../components/Day.js'
 export const runtime = 'experimental-edge';
-
-function dayCheck(wantDate) {
-  var now = new Date();
-  var writeDay = new Date(wantDate);
-  let ResultTime = "";
-
-  var difference = now.getTime() - writeDay.getTime();
-  difference = Math.trunc(difference / 1000);
-
-  const seconds = 1;
-  const minute = seconds * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const mon = day * 30;
-  const year = mon * 12;
-
-  let resultYear, resultMon, resultDay, resultHour, resultMin, resultSec;
-
-  resultYear = Math.trunc(difference / year);
-  resultMon = Math.trunc(difference / mon);
-  resultDay = Math.trunc(difference / day);
-  resultHour = Math.trunc(difference / hour);
-  resultMin = Math.trunc(difference / minute);
-  resultSec = Math.trunc(difference / seconds);
-
-  if(resultSec > 0){
-      ResultTime = resultSec+'초';
-      difference = difference - (resultSec * seconds );
-  }
-
-  if(resultMin > 0){
-      ResultTime = resultMin+'분';
-      difference = difference - (resultMin * minute);
-  }
-
-  if(resultHour > 0){
-      ResultTime = resultHour+'시간';
-      difference = difference - (resultHour * hour);
-  }
-
-  if(resultDay > 0){
-      ResultTime = resultDay+'일';
-      difference = difference - (resultDay * day);
-  }
-
-  if(resultMon > 0){
-      ResultTime = resultMon+'개월';
-      difference=  difference - (resultMon * mon);
-  }
-
-  if(resultYear > 0){
-    ResultTime = resultYear+'년';
-    difference= difference - (resultYear * year);
-  }
-
-  return ResultTime;
-}
 
 export default function ResolveRoute({data, url, title, description, fileData}) {
   if (data === undefined) {
@@ -105,7 +49,7 @@ export default function ResolveRoute({data, url, title, description, fileData}) 
                                     <div className='table_text'>
                                         <img src='/favicon-192x192.png' alt='logo' width='32px' height='32px' style={{borderRadius: '50%'}}/> <Link href="/">YSJ</Link> <p style={{marginLeft: '10px'}}>{data.commitMessage}</p>
                                     </div>
-                                    <div style={{textAlign: 'right', marginTop: '15px', marginLeft: '8px', marginRight: '8px'}}>{dayCheck(data.writeDay)} 전</div>
+                                    <div style={{textAlign: 'right', marginTop: '15px', marginLeft: '8px', marginRight: '8px'}}>{checkScript.dayCheck(data.writeDay)} 전</div>
                                 </th>
                             </tr>
                         </thead>
